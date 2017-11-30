ebc210acfc7ae80063da1201266e42448c1706e6
sadjorlolo <sarah.adjorlolo@libertymutual.com>
2017-11-29 19:04:28 -0500

    Add dependent destroy to events for invitees
    Alter invitees controller for invitee to delete invitation
    Alter event model for dependent: :destroy on invitees
    - if event is deleted, all invitee dependencies are deleted.
    Alter routes to remove update from invitees.

--- app/controllers/invitees_controller.rb
+++ app/controllers/invitees_controller.rb
@@ -11,48 +11,47 @@ class InviteesController < ProtectedController
 
   # GET /invitees/1
   def show
-    if current_user.invitees.find(params[:id])
       render json: @invitee
-    end
   end
 
   # POST /invitees
   def create
     # @invitee = Invitee.new(invitee_params)
     # The current user can only invite someone to an event they created
-    if current_user.created_events.find(invitee_params[:event_id]) && !Invitee.find_by(user_id: invitee_params[:user_id], event_id: invitee_params[:event_id])
+    if current_user.created_events.find(invitee_params[:event_id]) \
+       && !Invitee.find_by(user_id: invitee_params[:user_id], \
+                           event_id: invitee_params[:event_id])
       @invitee = Invitee.new(invitee_params)
-    end
+    # end
 
-    if @invitee.save
-      render json: @invitee, status: :created, location: @invitee
-    else
-      render json: @invitee.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /invitees/1
-  def update
-    # if current_user.created_events.find(invitee_params[:event_id]) || current_user == invitee_params[:user_id]
-    if current_user.invitees.find(params[:id])
-      if @invitee.update(invitee_params)
-        render json: @invitee
+      if @invitee.save
+        render json: @invitee, status: :created, location: @invitee
       else
         render json: @invitee.errors, status: :unprocessable_entity
       end
     end
   end
 
+  # PATCH/PUT /invitees/1
+  def update
+    # if current_user.created_events.find(invitee_params[:event_id]) || current_user == invitee_params[:user_id]
+      if @invitee.update(invitee_params)
+        render json: @invitee
+      else
+        render json: @invitee.errors, status: :unprocessable_entity
+      end
+  end
+
   # DELETE /invitees/1
   def destroy
-    if current_user.created_events.find(Invitee.find(params[:id]).event_id)
-      @invitee.destroy
-    # elsif current_user.invitees.find(params[:id])
+    # if current_user.created_events.find(Invitee.find(params[:id]).event_id)
     #   @invitee.destroy
-    end
+    # # elsif current_user.invitees.find(params[:id])
+    # #   @invitee.destroy
+    # end
 
     # if current_user.invitees.find(params[:id])
-    #   @invitee.destroy
+      @invitee.destroy
     # if current_user.invitees.find(params[:id])
     #   @invitee.destroy
     # end
@@ -93,11 +92,11 @@ class InviteesController < ProtectedController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_invitee
-      # @invitee = current_user.invitees.find(params[:id])
+      @invitee = current_user.invitees.find(params[:id])
       # @invitee2 = current_user.created_events.find(Invitee.find(params[:id]).event_id)
       # only if the current_user is the invitee, can record be deleted
       # if current_user.invitees.find(params[:id])
-      @invitee = Invitee.find(params[:id])
+      # @invitee = Invitee.find(params[:id])
       # end
     end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
   belongs_to :event_creator, class_name: 'User', foreign_key: 'user_id',
                              inverse_of: 'created_events'
 
-  has_many :invitees
+  has_many :invitees, dependent: :destroy
   has_many :event_attendees, through: :invitees
 end
