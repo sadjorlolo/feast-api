71a8b3462a9c6526512fbcf73f43f00982c21ec3
sadjorlolo <sarah.adjorlolo@libertymutual.com>
2017-11-30 19:17:43 -0500

    Remove commented out code from invitee model
    Delete commented out, unnecessary code from invitee model.

--- app/models/invitee.rb
+++ app/models/invitee.rb
@@ -1,5 +1,5 @@
 class Invitee < ApplicationRecord
-  belongs_to :event_attendee, class_name: 'User', foreign_key: 'user_id' #, inverse_of: 'attended_events'
+  belongs_to :event_attendee, class_name: 'User', foreign_key: 'user_id'
 
-  belongs_to :attended_event, class_name: 'Event', foreign_key: 'event_id' #, inverse_of: 'event_attendees'
+  belongs_to :attended_event, class_name: 'Event', foreign_key: 'event_id'
 end
